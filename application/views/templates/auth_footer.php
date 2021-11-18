4a164bd499b9b61568d6e77fdf21d99fdca5985e
Iman Abdurrahman <namasayaiman02@gmail.com>
2021-11-18 07:33:18 +0700

    Add files via upload

---
+++ application/views/templates/auth_footer.php
@@ -0,0 +1,15 @@
+<!-- Bootstrap core JavaScript-->
+<script src="<?= base_url('assets/vendor/jquery/jquery.min.js'); ?>"></script>
+<script src="<?= base_url('assets/vendor/bootstrap/js/bootstrap.bundle.min.js'); ?>"></script>
+
+<!-- Core plugin JavaScript-->
+<script src="<?= base_url('assets/vendor/jquery-easing/jquery.easing.min.js'); ?>"></script>
+
+<!-- Custom scripts for all pages-->
+<script src="<?= base_url('assets/js/sb-admin-2.min.js'); ?>"></script>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" crossorigin="anonymous"></script>
+<script src="<?= base_url('js/scripts.js'); ?>"></script>
+
+</body>
+
+</html>
---
+++ application/views/templates/footer.php
@@ -0,0 +1,103 @@
+<!-- Footer -->
+<footer class="sticky-footer bg-white">
+    <div class="container my-auto">
+        <div class="copyright text-center my-auto">
+            <span>Copyright &copy; Latihan Proyek <?= date('Y') ?></span>
+        </div>
+    </div>
+</footer>
+<!-- End of Footer -->
+
+</div>
+<!-- End of Content Wrapper -->
+
+</div>
+<!-- End of Page Wrapper -->
+
+<!-- Scroll to Top Button-->
+<a class="scroll-to-top rounded" href="#page-top">
+    <i class="fas fa-angle-up"></i>
+</a>
+
+<!-- Logout Modal-->
+<div class="modal fade" id="logoutModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+    <div class="modal-dialog" role="document">
+        <div class="modal-content">
+            <div class="modal-header">
+                <h5 class="modal-title" id="exampleModalLabel">Ready to Leave?</h5>
+                <button class="close" type="button" data-dismiss="modal" aria-label="Close">
+                    <span aria-hidden="true">×</span>
+                </button>
+            </div>
+            <div class="modal-body">Select "Logout" below if you are ready to end your current session.</div>
+            <div class="modal-footer">
+                <button class="btn btn-secondary" type="button" data-dismiss="modal">Cancel</button>
+                <a class="btn btn-primary" href="<?= base_url('auth/logout'); ?>">Logout</a>
+            </div>
+        </div>
+    </div>
+</div>
+
+<!-- Bootstrap core JavaScript-->
+<script src="<?= base_url('assets/vendor/jquery/jquery.min.js'); ?>"></script>
+<script src="<?= base_url('assets/vendor/bootstrap/js/bootstrap.bundle.min.js'); ?>"></script>
+
+<!-- Core plugin JavaScript-->
+<script src="<?= base_url('assets/vendor/jquery-easing/jquery.easing.min.js'); ?>"></script>
+
+<!-- Custom scripts for all pages-->
+<script src="<?= base_url('assets/js/sb-admin-2.min.js'); ?>"></script>
+
+<script>
+    var hoursLabel = document.getElementById("hours");
+    var minutesLabel = document.getElementById("minutes");
+    var secondsLabel = document.getElementById("seconds");
+    setInterval(setTime, 1000);
+
+    function setTime() {
+        secondsLabel.innerHTML = pad(Math.floor(new Date().getSeconds()));
+        minutesLabel.innerHTML = pad(Math.floor(new Date().getMinutes()));
+        hoursLabel.innerHTML = pad(Math.floor(new Date().getHours()));
+    }
+
+    function pad(val) {
+        var valString = val + "";
+        if (valString.length < 2) {
+            return "0" + valString;
+        } else {
+            return valString;
+        }
+    }
+
+    <?php if (@$this->session->absen_needed) : ?>
+        var absenNeeded = '<?= json_encode($this->session->absen_needed) ?>';
+        <?php $this->session->sess_unset('absen_needed') ?>
+    <?php endif; ?>
+
+    $('.custom-file-input').on('change', function() {
+        let fileName = $(this).val().split('\\').pop();
+        $(this).next('.custom-file-label').addClass("selected").html(fileName);
+    });
+
+
+    $('.form-check-input').on('click', function() {
+        const menuId = $(this).data('menu');
+        const roleId = $(this).data('role');
+
+        $.ajax({
+            url: "<?= base_url('admin/changeaccess') ?>",
+            type: 'post',
+            data: {
+                menuId: menuId,
+                roleId: roleId
+            },
+            success: function() {
+                document.location.href = "<?= base_url('admin/roleaccess/'); ?>" + roleId;
+            }
+        });
+    });
+</script>
+
+</body>
+
+</html>
